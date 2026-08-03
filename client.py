59326b8a99f65631cc4b6763de727db563c5f192
ajeje-brazorf-02 <ajejebrazorf_02@protonmail.com>
2026-08-03 16:32:43 +0000

    Add files via upload

--- client.py
+++ client.py
@@ -2,17 +2,32 @@ import cv2
 import socket
 import struct
 import pickle
+import select  # --- AGGIUNTO: serve per controllare comandi dal server senza bloccare il loop ---
 import imutils #type:ignore
 
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('93.45.38.163', 2444))
+client_socket.connect(('indirizzo ip del server', 2444))
 
 cam = cv2.VideoCapture(0)
 img_counter = 0
 encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
 
 while True:
+    # --- AGGIUNTO: controlla se il server ha inviato un comando di chiusura, senza bloccare il loop ---
+    ready_to_read, _, _ = select.select([client_socket], [], [], 0)
+    if ready_to_read:
+        header = b""
+        while len(header) < struct.calcsize(">L"):
+            chunk = client_socket.recv(struct.calcsize(">L") - len(header))
+            if not chunk:
+                break
+            header += chunk
+        if len(header) < struct.calcsize(">L") or struct.unpack(">L", header)[0] == 0:
+            print("Comando di chiusura ricevuto dal server, chiudo il client.")
+            break
+    # --- FINE AGGIUNTA ---
+
     ret, frame = cam.read()
     frame = imutils.resize(frame, width=320)
     frame = cv2.flip(frame,180)
@@ -24,3 +39,7 @@ while True:
         client_socket.sendall(struct.pack(">L", size) + data)
         
     img_counter += 1
+
+# --- AGGIUNTO: rilascia la webcam e chiude la connessione quando il loop termina ---
+cam.release()
+client_socket.close()
